3f76fc621bfc6eb602137f9ef0bf75d5526ccaa4
Derick Ostrenko <dostrenko@gmail.com>
2015-01-27 11:36:27 -0600

    heroku

--- data.js
+++ data.js
@@ -1,6 +1,12 @@
 // var redis = require("redis"),
 //     client = redis.createClient();
 
+var redis = require('redis');
+var url = require('url');
+var redisURL = url.parse(process.env.REDISCLOUD_URL);
+var client = redis.createClient(redisURL.port, redisURL.hostname, {no_ready_check: true});
+client.auth(redisURL.auth.split(":")[1]);
+
 client.on("error", function(err) {
     console.log("Error " + err);
 });
