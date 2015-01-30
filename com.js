be3ef10c04a9245b4b322d42ce696dc158b94fdf
Derick Ostrenko <dostrenko@gmail.com>
2015-01-30 13:59:09 -0600

    livereload

--- com.js
+++ com.js
@@ -9,23 +9,8 @@ var logger = require('morgan');
 var redis = require('redis');
 var url = require('url');
 
-// var redisURL = url.parse(process.env.REDISCLOUD_URL);
-// var client = redis.createClient(redisURL.port, redisURL.hostname, {
-//     no_ready_check: true
-// });
-// client.auth(redisURL.auth.split(":")[1]);
-
-
-
-
-
-
-
-
-
 var redisUrl;
 var client;
-// var redisUrlDefault = 'redis://:@127.0.0.1:6379/0';
 
 redisConnect();
 
@@ -41,19 +26,8 @@ function redisConnect() {
 	}
 	else { 
 		client = redis.createClient();
-		// redisUrl = url.parse(redisUrlDefault);
 	}
 }
-
-
-
-
-
-
-
-
-
-
 
 var usgsdata = require("./data.js");
 
